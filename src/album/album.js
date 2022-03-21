536a121384ac8296297800e99c5b0567f78631db
arkaanni <arkaannuruliman@gmail.com>
2022-03-21 13:00:37 +0700

    get album by id

--- src/album/album.js
+++ src/album/album.js
@@ -1,10 +1,17 @@
 const endpoint = 'http://jsonplaceholder.typicode.com/albums'
+
 const getAlbumByUserId = (userId) => {
     return fetch(`${endpoint}?userId=${userId}`, {
         method: 'GET'
     }).then(res => res.json())
 }
 
+const getAlbumById = (id) => {
+    return fetch(`${endpoint}/${id}`, {
+        method: 'GET'
+    }).then(res => res.json())
+}
+
 export {
-    getAlbumByUserId
+    getAlbumByUserId, getAlbumById
 }
